f6d69246ce247dc2dba587c8cbeed9ba7021c125
Kinley Tshering <kinleytshering@Kinleys-MacBook-Pro.local>
2021-08-07 18:10:53 +0600

    This is for testing

---
+++ src/main/java/com/test/digitalkidu/DigitalkiduApplication.java
@@ -0,0 +1,15 @@
+package com.test.digitalkidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalkiduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalkiduApplication.class, args);
+
+		System.out.println("Hello Digital Kidu");
+	}
+
+}
---
+++ src/test/java/com/test/digitalkidu/DigitalkiduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.digitalkidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalkiduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
